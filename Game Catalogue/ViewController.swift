b2e0a3bf90b3312a5a8193977f729dec30cc0483
Miky Setiawan <mikysetiawan@gmail.com>
2020-07-05 01:09:47 +0700

    Initial Commit

---
+++ Game Catalogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Catalogue
+//
+//  Created by Miky Setiawan on 05/07/20.
+//  Copyright © 2020 Miky Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
